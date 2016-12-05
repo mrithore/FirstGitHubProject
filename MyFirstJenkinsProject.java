2a78c02eee5fe23e622e10d3007fdedccec5fa87
mrithore <mritunjoy_hore@rediffmail.com>
2016-12-05 19:37:24 +0530

    New files added

--- MyFirstJenkinsProject.java
+++ MyFirstJenkinsProject.java
@@ -1,7 +1,8 @@
 
 public class MyFirstJenkinsProject {
 	
-	public static void main(String args[]){
+	public static void main(String args[])
+	{
 		System.out.println("Jenkins Integration with Jmeter using Github as SCM");
 	}
 
